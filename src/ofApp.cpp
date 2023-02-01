341092d3155ce0606fb665e1d6b6270970c9c37a
Jenny <y.jiang0220224@arts.ac.uk>
2023-02-01 13:21:33 +0000

    Add project files.

---
+++ src/ofApp.cpp
@@ -0,0 +1,87 @@
+#pragma once
+#include "boid.h"
+#include "neoBoid.h"
+#include "ofApp.h"
+
+ofApp::~ofApp()
+{
+	for (int i = 0; i < boids.size(); i++)
+	{
+		delete boids[i];
+	}
+}
+
+//--------------------------------------------------------------
+void ofApp::setup() {
+
+
+	int screenW = ofGetScreenWidth();
+	int screenH = ofGetScreenHeight();
+
+	ofBackground(0, 50, 50);
+
+	// set up the boids
+	for (int i = 0; i < 50; i++) {
+		boids.push_back(new Boid());
+		newBoids.push_back(new neoBoids());
+	}
+
+}
+
+
+//--------------------------------------------------------------
+void ofApp::update() {
+
+	ofVec3f min(0, 0);
+	ofVec3f max(ofGetWidth(), ofGetHeight());
+	for (int i = 0; i < boids.size(); i++)
+	{
+		boids[i]->update(boids, min, max);
+	}
+}
+
+//--------------------------------------------------------------
+void ofApp::draw() {
+
+	for (int i = 0; i < boids.size(); i++)
+	{
+		boids[i]->draw();
+	}
+
+}
+
+
+//--------------------------------------------------------------
+void ofApp::keyPressed(int key) {
+
+}
+
+//--------------------------------------------------------------
+void ofApp::keyReleased(int key) {
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseMoved(int x, int y) {
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseDragged(int x, int y, int button) {
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mousePressed(int x, int y, int button) {
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseReleased(int x, int y, int button) {
+
+}
+
+//--------------------------------------------------------------
+void ofApp::windowResized(int w, int h) {
+
+}
